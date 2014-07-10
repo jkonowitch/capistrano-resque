11c3ebb85784aae9c6a3d51dec39bdc3a0bdd262
Jeffrey Konowitch <jkonowitch@gmail.com>
2014-07-10 15:15:46 -0400

    Update capistrano_integration.rb

--- lib/capistrano-resque/capistrano_integration.rb
+++ lib/capistrano-resque/capistrano_integration.rb
@@ -40,7 +40,7 @@ module CapistranoResque
 
         def start_command(queue, pid)
           "cd #{current_path} && RAILS_ENV=#{rails_env} QUEUE=\"#{queue}\" \
-           PIDFILE=#{pid} BACKGROUND=yes VERBOSE=1 INTERVAL=#{interval} \
+           PIDFILE=#{pid} BACKGROUND=yes INTERVAL=#{interval} \
            #{fetch(:bundle_cmd, "bundle")} exec rake \
            #{"environment" if fetch(:resque_environment_task)} \
            resque:work #{output_redirection}"
